4b88e359836790a70a1cd78678eab1e6606aed98
patrikHjortberg <pathjo0826@skola.goteborg.se>
2022-06-19 20:47:02 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,119 @@
+public class Main {
+
+    private static final int GRID_SIZE = 9;
+
+    public static void main(String[] args) {
+
+        int[][] board = {
+                {7, 0, 2, 0, 5, 0, 6, 0, 0},
+                {0, 0, 0, 0, 0, 3, 0, 0, 0},
+                {1, 0, 0, 0, 0, 9, 5, 0, 0},
+                {8, 0, 0, 0, 0, 0, 0, 9, 0},
+                {0, 4, 3, 0, 0, 0, 7, 5, 0},
+                {0, 9, 0, 0, 0, 0, 0, 0, 8},
+                {0, 0, 9, 7, 0, 0, 0, 0, 5},
+                {0, 0, 0, 2, 0, 0, 0, 0, 0},
+                {0, 0, 7, 0, 4, 0, 2, 0, 3}
+        };
+
+        System.out.println("Original Sudoko board");
+        printBoard(board);
+
+        if (solveBoard(board)) {
+            System.out.println("Solved succesfully!");
+            printBoard(board);
+        } else {
+            System.out.println("Impossible to solve");
+        }
+    }
+
+
+    public static boolean isNumberInRow(int[][] board, int number, int row) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[row][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isNumberInColumn(int[][] board, int number, int column) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[i][column] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
+        /*
+        Given a coordinate in the Sudoku grid, the formulas below will give the top left corner
+        of the 3x3 grid that the coordinate is in. Try it!
+         */
+
+        int localBoxRow = row - row % 3;
+        int localBoxColumn = column - column % 3;
+
+        for (int i = localBoxRow; i < localBoxRow + 3; i++) {
+            for (int j = localBoxColumn; j < localBoxColumn + 3; j++) {
+                if (board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
+
+        return !isNumberInRow(board, number, row) &&
+                !isNumberInColumn(board, number, column) &&
+                !isNumberInBox(board, number, row, column);
+    }
+
+    private static boolean solveBoard(int[][] board) {
+
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int column = 0; column < GRID_SIZE; column++) {
+                if (board[row][column] == 0) {
+                    for (int number = 1; number <= 9; number++) {
+                        if (isValidPlacement(board, number, row, column)) {
+                            board[row][column] = number;
+
+                            /*
+                            Recursively calls itself. This is the "back-track" function. If a previous placement
+                            makes the board unsolvable it goes back and tries the next valid placement in that coordinate.
+
+                            The else-statement resets the coordinates solveBoard() has filled in if it eventually runs into a
+                            situation where it finds no valid placements.
+                             */
+
+                            if (solveBoard(board)) {
+                                return true;
+                            } else {
+                                board[row][column] = 0;
+                            }
+                        }
+                    }
+                    /*
+                    Returns false if all numbers 1-9 has been tried and no validPlacement has been found.
+                    = Unsolvable board.
+                     */
+
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
